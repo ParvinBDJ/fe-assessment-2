cf2c46b76d39fb25337e0b83f0f3d1aebb0222d9
ParvinBDJ <55492595+ParvinBDJ@users.noreply.github.com>
2020-06-23 02:37:32 +0200

    Add files via upload

--- java.js
+++ java.js
@@ -1,8 +1,12 @@
-console.log('hoi');
-
 const cursor = document.querySelector('.cursor');
 const link = document.querySelectorAll('a');
 const inactive = 'inactive';
+const text = document.querySelector("#fade");
+const textHTML = text.textContent;
+const splitText = textHTML.split("");
+const nameValidation = document.getElementById("firstname");
+const form = document.getElementById("nameform");
+const error = document.getElementById("error");
 
 function moveCursor(e){
 
@@ -16,4 +20,41 @@ function moveCursor(e){
 
 
 document.addEventListener("mousemove", moveCursor);
+text.textContent = "";
+// Before we run this function we delete the original text within the H1
+const save = splitText.map(x => text.innerHTML += "<span>" + x + "</span>");
+// here i take every letter of the H1 and put it in a seperate span
+
+let char = 0;
+let timer = setInterval(whenOpen, 50);
+
+function whenOpen(){
+    const span = document.querySelectorAll("span")[char];
+    span.classList.add('fade');
+    char++;
+    if(char === splitText.length){
+        complete();
+        return;
+    }
+}
+
+function complete(){
+    clearInterval(timer);
+    timer = null;
+}
+
+if (form){
+    form.addEventListener('submit', function validate(e){
+        console.log(nameValidation.value.length);
+        if(nameValidation.value.length < 4){
+            error.innerHTML = "must be at least 4 charachters";
+            console.log("error")
+            e.preventDefault();
+        } else{
+            console.log("lekker bezig!");
+        }
+    })
+} else{ 
+    console.log("no form to validate")
+}
 
